c6184d3c73d6c6b68609ac0c552df51532543d96
Andrei Galanton <72964493+AndreiGalanton@users.noreply.github.com>
2021-11-19 14:32:51 +0200

    Update gan.py

--- procesareDeImagini/imgDatasetProcessed/gan.py
+++ procesareDeImagini/imgDatasetProcessed/gan.py
@@ -17,16 +17,16 @@ import torch
 os.makedirs("images", exist_ok=True)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=10000, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+parser.add_argument("--n_epochs", type=int, default=5000, help="number of epochs of training")
+parser.add_argument("--batch_size", type=int, default=128, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-parser.add_argument("--img_size", type=int, default=100, help="size of each image dimension")
+parser.add_argument("--img_size", type=int, default=200, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=3, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
+parser.add_argument("--sample_interval", type=int, default=50, help="interval betwen image samples")
 opt = parser.parse_args()
 print(opt)
 
@@ -34,30 +34,6 @@ img_shape = (opt.channels, opt.img_size, opt.img_size)
 
 cuda = True if torch.cuda.is_available() else False
 
-
-class facadeDataset(Dataset):
-    
-    #initializare clasa cu numPyArray-ul create la Partea 1
-    def __init__(self, finalNumpyArray):
-        self.finalNumpyArray = finalNumpyArray
-        
-    #functie care returneaza numarul de elemente din Dataset        
-    def __len__(self):        
-        return len(self.finalNumpyArray)
-    
-    #functie care returneaza un item din Dataset
-    def __getitem__(self, index):
-        
-        image =  self.finalNumpyArray[index]
-        result = self.transform(image)
-        return result
-    
-    transform = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize(size=(opt.img_size,opt.img_size)),
-        #T.RandomResizedCrop(image_size),
-        #T.RandomHorizontalFlip(),
-        transforms.ToTensor()])
 
 
 class Generator(nn.Module):
